4c56f31568fd3cbd9d6f2a4da262959d853bffd2
Muku18 <88443590+Muku18@users.noreply.github.com>
2023-03-27 23:44:57 +0530

    Update app.py
    second commit

--- app.py
+++ app.py
@@ -1,89 +1,84 @@
-# Importing Modules
-from flask import Flask
-from flask_pymongo import PyMongo
-from bson.json_util import dumps
-from bson.objectid import ObjectId
-from flask import jsonify,request
+from flask import Flask, request,jsonify
+from flask_restful import Api, Resource, reqparse, fields, marshal_with
+from pymongo import MongoClient
+from bson import ObjectId
 from werkzeug.security import generate_password_hash
 
-
-# Initializing and Connecting to Database
 app = Flask(__name__)
-app.secret_key = "secretkey"
-app.config['MONGO_URI'] =  "mongodb://localhost:27017/Users"
-mongo = PyMongo(app)
+api = Api(app)
 
-# Creates a new user with the specified data.
-@app.route('/users',methods = ['POST'])
-def create_user():
-    json = request.json
-    _name = json['name']
-    _email = json['email']
-    _pass = json['pwd']
-    if _name and _email and _pass and request.method == 'POST':
+# MongoDB connection
+client = MongoClient('mongodb://localhost:27017/')
+db = client['Users']
+mongo = db['user']
+
+# User resource fields
+user_fields = {
+    'id': fields.String(attribute='_id'),
+    'name': fields.String,
+    'email': fields.String,
+    'password': fields.String,
+}
+
+# User resource parser
+user_parser = reqparse.RequestParser()
+user_parser.add_argument('name', type=str, required=True, help='Name is required')
+user_parser.add_argument('email', type=str, required=True, help='Email is required')
+user_parser.add_argument('password', type=str, required=True, help='Password is required')
+
+class AllUser(Resource):
+    @marshal_with(user_fields)
+    def get(self):
+        return list(mongo.find()),200
+
+    @marshal_with(user_fields)
+    def post(self, ):
+        # Create a new user
+        args = user_parser.parse_args()
+        _pass = args['password']
         hashed_pwd = generate_password_hash(_pass)
-        id = mongo.db.user.insert_one({'name':_name,'email':_email,'pwd':hashed_pwd})
-        res = jsonify("User added Successfully")
-        res.status_code = 200
-        return res
-    else:
-        return details_not_found()
-
-# Returns a list of all users.
-@app.route('/users',methods = ['GET'])
-def displayusers():
-    users = mongo.db.user.find()
-    res = dumps(users)
-    return res
-
-# Returns the user with the specified ID.
-@app.route('/users/<id>',methods = ['GET'])
-def specificuser(id):
-    user = mongo.db.user.find_one({'_id':ObjectId(id)})
-    res = dumps(user)
-    return res
+        user = {'name': args['name'], 'email': args['email'], 'password': hashed_pwd}
+        user_id = mongo.insert_one(user)
+        if user_id.acknowledged:
+            return {'message': 'User created', 'id': str(user_id.inserted_id)}, 201
+        else:
+            return {'message': 'Error creating user'}, 500
+class User(Resource):
+    @marshal_with(user_fields)
+    def get(self, user_id):
+        user = mongo.find_one({'_id': ObjectId(user_id)})
+        if user:
+            user['_id'] = str(user['_id'])
+            return jsonify(user)
+        else:
+            return jsonify({'message': 'User not found'}), 404
 
 
-# Updates the user with the specified ID with the new data.
-@app.route('/users/<id>',methods = ['PUT'])
-def updatespecificuser(id):
-    _id = id
-    json = request.json
-    _name = json['name']
-    _email = json['email']
-    _pass = json['pwd']
-    if _name and _email and _pass and _id and request.method == 'PUT':
+    @marshal_with(user_fields)
+    def put(self, user_id):
+        # Update a user by id
+        args = user_parser.parse_args()
+        _pass = args['password']
         hashed_pwd = generate_password_hash(_pass)
-        mongo.db.user.update_one({'_id': ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)},
-                                 {'$set':{'name':_name,'email':_email,'pwd':hashed_pwd}})
-        res = jsonify("User updated Successfully")
-        res.status_code = 200
-        return res
-    else:
-        return details_not_found()
+        user = {'name': args['name'], 'email': args['email'], 'password': hashed_pwd}
+        result = mongo.update_one({'_id': ObjectId(user_id)}, {'$set': user})
+        if result.modified_count:
+            return jsonify({'message': 'User updated'})
+        else:
+            return jsonify({'message': 'User not found'}), 404
 
-# Deletes the user with the specified ID.
-@app.route('/users/<id>',methods = ['DELETE'])
-def deletespecificuser(id):
-    mongo.db.user.delete_one({'_id':ObjectId(id)})
-    res = jsonify("User deleted Successfully")
-    res.status_code = 200
-    return res
+    def delete(self, user_id):
+        result = mongo.delete_one({'_id': ObjectId(user_id)})
+        if result.deleted_count:
+            return jsonify({'message': 'User deleted'})
+        else:
+            return jsonify({'message': 'User not found'}), 404
 
 
+api.add_resource(AllUser, '/users')
+api.add_resource(User, '/users/<string:user_id>')
 
-# Error handler function
-@app.errorhandler(404)
-def details_not_found(error = None):
-    message = {
-        'status' : 404,
-        'message' : 'Details Not Found' + request.url
-    }
-    res = jsonify(message)
-    res.status_code = 200
-    return res
+if __name__ == '__main__':
+    app.run(debug=True)
 
-
-if __name__ == "__main__":
-    app.run(debug = True)
 
